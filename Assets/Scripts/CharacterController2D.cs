9969834875e17e089a6453d90165ada3d2985805
H1ddenGames <skarim3000@gmail.com>
2019-05-19 02:12:48 -0700

    Added player movement, recreated the environment, updated README
    Finished implementing player movement.
    Finished implementing camera follow.
    Currently working on player animation.
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -7,10 +7,10 @@ public class CharacterController2D : MonoBehaviour
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
-	[SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
-	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
-	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+	[SerializeField] private LayerMask m_WhatIsGround = 0;							// A mask determining what is ground to the character
+	[SerializeField] private Transform m_GroundCheck = null;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck = null;							// A position marking where to check for ceilings
+	[SerializeField] private Collider2D m_CrouchDisableCollider = null;				// A collider that will be disabled when crouching
 
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
