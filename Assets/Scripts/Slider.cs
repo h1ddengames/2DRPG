6844dd7fc520726436b7f3e22bc793ba7d42ee13
H1ddenGames <skarim3000@gmail.com>
2019-06-04 00:17:42 -0700

    Added UI elements and assets, completed Stat system
    Created a menu UI with animation.
    Added pixel UI assets.
    Implemented a relatively simple stat system that utilizes the delegate pattern for updating rather than using the update method.
    
    Need to implement a better UI design in order to display stats.
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- Assets/Scripts/Slider.cs
+++ Assets/Scripts/Slider.cs
@@ -7,18 +7,51 @@ namespace h1ddengames.twodrpg.ui
     [ExecuteInEditMode]
     public class Slider : MonoBehaviour
     {
-        [SerializeField, Range(0f, 1f)] private float fillAmount;
-        [SerializeField] private string valueText;
+        public delegate void SliderDelegate();
+        public static event SliderDelegate sliderHasChangedEvent;
+        [SerializeField, Range(0f, 1f)] private float _fillAmount;
+        [SerializeField] private string _valueText;
 
         [SerializeField] private Image fill; 
         [SerializeField] private TextMeshProUGUI text; 
         
-        public float FillAmount { get => fillAmount; set => fillAmount = value; }
-        public string ValueText { get => valueText; set => valueText = value; }
+        public float FillAmount { 
+            get => _fillAmount; 
+            set {
+                if(value != _fillAmount && value >= 0 && value <= 1) { 
+                    _fillAmount = value;
+                    if(sliderHasChangedEvent != null) sliderHasChangedEvent(); 
+                } else { return; }
+            }  
+        }
+
+        public string ValueText { 
+            get => _valueText; 
+            set {
+                if(value != _valueText) { 
+                    _valueText = value;
+                    if(sliderHasChangedEvent != null) sliderHasChangedEvent(); 
+                } else { return; }
+            }
+        }
+
         public Image Fill { get => fill; set => fill = value; }
         public TextMeshProUGUI Text { get => text; set => text = value; }
 
+        private void OnEnable() {
+            sliderHasChangedEvent += OnSliderChanged;
+            UpdateSlider();
+        }
+
         private void OnValidate() {
+            UpdateSlider();
+        }
+
+        public void OnSliderChanged() {
+            UpdateSlider();
+        }
+
+        public void UpdateSlider() {
             if(Fill != null) {
                 Fill.fillAmount = FillAmount;
             }
---
+++ Assets/Scripts/StatsSO.cs
@@ -0,0 +1,62 @@
+﻿namespace h1ddengames.twodrpg.statistics
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Stats", menuName = "2DRPG/Stats", order = 0)]
+    public class StatsSO : ScriptableObject
+    {
+        [SerializeField] private Stats _stats;
+        [SerializeField, Header("Randomized Stats")] private int _strengthMin = 0; 
+        [SerializeField] private int _strengthMax = 0, _intelligenceMin = 0, _intelligenceMax = 0, _dexterityMin = 0, 
+        _dexterityMax = 0, _luckMin = 0, _luckMax = 0;
+        [SerializeField] private int _healthPointsMin = 0, _healthPointsMax = 0, _manaPointsMin = 0, _manaPointsMax = 0;
+        [SerializeField] private int _attackPointsMin = 0, _attackPointsMax = 0, _magicAttackPointsMin = 0, _magicAttackPointsMax = 0;
+        [SerializeField] private int _experienceMin = 0, _experienceMax = 0;
+
+        public Stats GetSetStats { get => _stats; set => _stats = value; }
+
+        #region Constructor
+        public StatsSO() { GetSetStats = new Stats(); }
+        public StatsSO(Stats stats) { GetSetStats = stats; }
+        #endregion
+
+
+        public void UpdateDatabase() {
+            // Open the connection to the database.
+
+            // Check that the current stats have not already been added.
+            // Create the query.
+
+            // Send the query.
+
+            // If the resultset states no entry matches the query then
+                // Create the statement.
+
+                // Send the statement.
+
+            // Close the connection to the database.
+
+        }
+
+        public void CreateJSONBackup() {
+            // Use JSONify to create a JSON object.
+
+            // Save the JSON object to a file where users expect their game data to be saved.
+        }
+
+        [ContextMenu("Randomize Stats")]
+        public void RandomizeStats() {
+            GetSetStats.Strength = Random.Range(_strengthMin, _strengthMax);
+            GetSetStats.Intelligence = Random.Range(_intelligenceMin, _intelligenceMax);
+            GetSetStats.Dexterity = Random.Range(_dexterityMin, _dexterityMax);
+            GetSetStats.Luck = Random.Range(_luckMin, _luckMax);
+            GetSetStats.HealthPoints = Random.Range(_healthPointsMin, _healthPointsMax);
+            GetSetStats.ManaPoints = Random.Range(_manaPointsMin, _manaPointsMax);
+            GetSetStats.AttackPoints = Random.Range(_attackPointsMin, _attackPointsMax);
+            GetSetStats.MagicAttackPoints = Random.Range(_magicAttackPointsMin, _magicAttackPointsMax);
+            GetSetStats.Experience = Random.Range(_experienceMin, _experienceMax);
+        }
+    }
+}
