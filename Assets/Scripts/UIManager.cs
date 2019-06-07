8dacd5ec5ad4ebb27098a0ec1ea9dc72f3bc1a18
H1ddenGames <skarim3000@gmail.com>
2019-06-07 01:18:44 -0700

    Updated the UI, changed scripts
    The UI now uses Pixel Art.
    The Slider will now display 0 fill when the player's health is below 0.
    
    In progress:
    Update EntityStats to have all variables with full getters/setters.
    Update UIManager to have all the variables required to display the stats on the UI.
    Update the UI to contain the Stat window.
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,12 +6,15 @@ namespace h1ddengames.twodrpg.managers
     using h1ddengames.twodrpg.statistics;
     
     public class UIManager : MonoBehaviour {
-        [SerializeField] private h1ddengames.twodrpg.ui.Slider healthSlider; 
-        [SerializeField] private h1ddengames.twodrpg.ui.Slider manaSlider; 
+        [SerializeField] private h1ddengames.twodrpg.ui.Slider _healthSlider; 
+        [SerializeField] private h1ddengames.twodrpg.ui.Slider _manaSlider; 
+        [SerializeField] private h1ddengames.twodrpg.ui.Slider _expSlider;
+        [SerializeField] private TextMeshProUGUI _playerName;
         [SerializeField] private EntityStats playerStats; 
 
-        public Slider HealthSlider { get => healthSlider; set => healthSlider = value; }
-        public Slider ManaSlider { get => manaSlider; set => manaSlider = value; }
+        public Slider HealthSlider { get => _healthSlider; set => _healthSlider = value; }
+        public Slider ManaSlider { get => _manaSlider; set => _manaSlider = value; }
+        public Slider ExperienceSlider { get => _expSlider; set => _expSlider = value; }
         public EntityStats PlayerStats { get => playerStats; set => playerStats = value; }
         
         private void OnEnable() {
@@ -24,15 +27,21 @@ namespace h1ddengames.twodrpg.managers
         }
 
         public void UpdateUI() {
-            if(!(PlayerStats.MaxHealthPoints == 0)) {
-                HealthSlider.FillAmount = (float) PlayerStats.CurrentHealthPoints/PlayerStats.MaxHealthPoints;
-                HealthSlider.ValueText = PlayerStats.CurrentHealthPoints.ToString() + "/" + PlayerStats.MaxHealthPoints.ToString();
-            }
+            if(PlayerStats.CurrentHealthPoints > 0) {
+                HealthSlider.FillAmount = (float) PlayerStats.CurrentHealthPoints/PlayerStats.MaxHealthPoints;   
+            } else if(PlayerStats.CurrentHealthPoints <= 0) { HealthSlider.FillAmount = 0; }
 
-            if(!(PlayerStats.MaxManaPoints == 0)) {
+            HealthSlider.ValueText = PlayerStats.CurrentHealthPoints.ToString() + "/" + PlayerStats.MaxHealthPoints.ToString();
+
+            if(PlayerStats.CurrentManaPoints > 0) {
                 ManaSlider.FillAmount = (float) PlayerStats.CurrentManaPoints/PlayerStats.MaxManaPoints;
-                ManaSlider.ValueText = PlayerStats.CurrentManaPoints.ToString() + "/" + PlayerStats.MaxManaPoints.ToString();
-            }
+            } else if (PlayerStats.CurrentManaPoints <= 0) { ManaSlider.FillAmount = 0; }
+            
+            ManaSlider.ValueText = PlayerStats.CurrentManaPoints.ToString() + "/" + PlayerStats.MaxManaPoints.ToString();
+
+            if(PlayerStats.CurrentExperience > 0) {
+                // ExpSlider.FillAmount = (float) PlayerStats.CurrentExperience/PlayerStats.ExperienceCurve.Evaluate(PlayerStats.Level);
+            } else if (PlayerStats.CurrentExperience <= 0) { ExperienceSlider.FillAmount = 0; }
         }
     }
 }
